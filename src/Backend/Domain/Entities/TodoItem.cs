67ac4d1a1cc38fda4c840f320707c7fc2bf4fc36
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2025-11-14 17:25:41 -0300

    Nuevo proyecto
    Capas:
    - dominio
    -  infra
    - aplicacion
    - presentacion
    
    Clases:
    -Todoitem
    -TodoitemDto
    -Todostatus
    -User
    -UserDto
    
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

---
+++ src/Backend/Domain/Entities/TodoItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public TodoStatus Status { get; set; }
+        public bool IsCompleted { get; set; } = false;
+
+        public Guid UserId { get; set; }
+
+        //navigation property
+        public User User { get; set; }
+
+        public void MakeCompleted()
+        {
+            if (Status == TodoStatus.Completed)
+            {
+                IsCompleted = true;
+            }
+        }
+
+        public void ChangeStatus(TodoStatus status)
+        {
+            Status = status;
+        }
+    }
+
+    
+}
---
+++ src/Backend/Domain/Interfaces/ITodoRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface ITodoRepository
+    {
+        Task<TodoItem?> GetByIdAsync(Guid id);
+
+        Task<IEnumerable<TodoItem>> GetByUserIdAsync(Guid id);
+
+        Task<IEnumerable<TodoItem>> GetAllAsync();
+
+        Task<TodoItem> CreateAsync(TodoItem entity);
+
+        Task<TodoItem> UpdateAsync(TodoItem entity);
+
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
