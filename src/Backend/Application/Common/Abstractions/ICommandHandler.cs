0d3c79dedd114c06d2908e9a4498fc2f08c9a568
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2025-11-16 03:15:47 -0300

    Created abstractions for commands
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

---
+++ src/Backend/Application/Common/Abstractions/ICommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using ErrorOr;
+
+namespace Application.Common.Abstractions
+{
+    /// <summary>
+    /// Interfaz para el handle de cada comando
+    /// </summary>
+    /// <typeparam name="TCommand"></typeparam>
+    public interface ICommandHandler<in TCommand>
+            where TCommand : ICommand
+    {
+        Task<ErrorOr<Success>> HandleAsync(TCommand command,
+            CancellationToken cancellationToken);
+    }
+
+    public interface ICommandHandler<in TCommand, TResponse>
+        where TCommand : ICommand<TResponse>
+    {
+        Task<ErrorOr<TResponse>> HandleAsync(TCommand command,
+            CancellationToken cancellationToken);
+    }
+}
