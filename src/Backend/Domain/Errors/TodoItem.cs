1e6b053fe112be42f9c6ed4300676e81c08dc623
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2025-11-18 03:05:02 -0300

    Created the error parts for the entities

---
+++ src/Backend/Domain/Errors/TodoItem.cs
@@ -0,0 +1,47 @@
+﻿using ErrorOr;
+
+namespace Domain.Errors;
+
+public static partial class DomainErrors
+{
+    /// <summary>
+    /// Clase estática para definir y acceder a los errores relacionados con la entidad TodoItem.
+    /// </summary>
+    public static class TodoItem
+    {
+        public static Error TitleTooLong() =>
+            Error.Validation(
+                code: "TodoItem.Title",
+                description: "The title cannot be over 100 characters.");
+
+        public static Error TitleEmptyOrWhiteSpace() =>
+            Error.Validation(
+                code: "TodoItem.Title",
+                description: "The title cannot be empty");
+
+        public static Error InvalidUserId() =>
+            Error.Validation(
+                code: "TodoItem.InvalidUser",
+                description: "The user id is not valid or empty");
+
+        public static Error InvalidDate() =>
+            Error.Validation(
+                code: "TodoItem.LimitDate",
+                description: "The date cannot be previous from today");
+
+        public static Error CompleteACancelledTask() =>
+            Error.Conflict(
+                code: "TodoItem.Cancelled",
+                description: "A cancelled task cannot be completed");
+
+        public static Error StartProgressACancelledTask() =>
+            Error.Conflict(
+                code: "TodoItem.Status",
+                description: "A cancelled task cannot be in progress");
+
+        public static Error CancelACompleteTask() =>
+            Error.Conflict(
+                code: "TodoItem.Status",
+                description: "A completed task cannot be cancelled");
+    }
+}
---
+++ src/Backend/Domain/Errors/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ErrorOr;
+
+namespace Domain.Errors
+{
+    public static partial class DomainErrors
+    {
+        /// <summary>
+        /// Clase estática para definir y acceder a los errores relacionados con la entidad User.
+        /// </summary>
+        public static class User
+        {
+            public static Error UserNotFound() =>
+                Error.NotFound(
+                    code: "User.NotFound",
+                    description: "The user was not found.");
+        }
+    }
+}
