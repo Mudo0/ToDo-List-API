1e54eb5dead7b590eefaecfeee7b2215f23bf49d
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2025-11-18 00:13:45 -0300

    Created abstractions for querys

---
+++ src/Backend/Application/Common/Abstractions/IQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Abstractions
+{
+    /// <summary>
+    /// Interfaz para las consultas.
+    /// </summary>
+    /// <typeparam name="TResponse">Tipo de respuesta esperada de la consulta</typeparam>
+    public interface IQuery<TResponse>
+    {
+    }
+}
---
+++ src/Backend/Application/Common/Abstractions/IQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using ErrorOr;
+
+namespace Application.Common.Abstractions;
+
+/// <summary>
+///Interfaz para el handler de cada consulta.
+/// </summary>
+/// <typeparam name="TQuery">Tipo de consulta a manejar</typeparam>
+/// <typeparam name="TResponse">Tipo de respuesta que devuelve</typeparam>
+public interface IQueryHandler<in TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+{
+    /// <summary>
+    /// Maneja la consulta específica y devuelve una respuesta o un error.
+    /// </summary>
+    /// <param name="query">Instancia de la consulta a manejar</param>
+    /// <param name="cancellationToken">Token de cancelación para la transacción asíncrona</param>
+    /// <returns></returns>
+    Task<ErrorOr<TResponse>> HandleAsync(TQuery query, CancellationToken cancellationToken);
+}
