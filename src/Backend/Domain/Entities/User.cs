cac5b2d3466b89602238726d20c47a89cd7a7ac7
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2025-11-18 02:03:04 -0300

    Added factory method to User

--- src/Backend/Domain/Entities/User.cs
+++ src/Backend/Domain/Entities/User.cs
@@ -1,12 +1,29 @@
-﻿namespace Domain.Entities
+﻿using ErrorOr;
+
+namespace Domain.Entities
 {
     /// <summary>
     /// Entidad del dominio que representa a un usuario.
     /// </summary>
     public class User
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; private set; }
 
-        public virtual ICollection<TodoItem>? TodoItems { get; set; } = new List<TodoItem>();
+        public virtual ICollection<TodoItem>? TodoItems { get; private set; } = [];
+
+        private User()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// Método fabrica para crear una nueva instancia de User.
+        /// </summary>
+        /// <returns>Un nuevo usuario anónimo</returns>
+        public static ErrorOr<User> Create()
+        {
+            var user = new User();
+            return user;
+        }
     }
 }
