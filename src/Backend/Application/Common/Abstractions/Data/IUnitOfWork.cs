4caca3501acca96afe1639cdeeb2b78e30697240
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2025-11-21 15:27:50 -0300

    created unitofwork
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

---
+++ src/Backend/Application/Common/Abstractions/Data/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Abstractions.Data
+{
+    /// <summary>
+    /// Contrato para la unidad de trabajo que maneja las transacciones de la base de datos.
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        //método a exponer para el handler
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
