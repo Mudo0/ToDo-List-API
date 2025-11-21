418e258660172bad55708e0f61bdd996848b1fdd
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2025-11-21 15:27:12 -0300

    Added application errors
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

---
+++ src/Backend/Application/Common/Abstractions/Errors/ApplicationErrors.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Common.Abstractions.Errors
+{
+    /// <summary>
+    /// Clase estática parcial para definir errores de la capa de aplicación.
+    /// </summary>
+    public static partial class ApplicationErrors
+    {
+    }
+}
---
+++ src/Backend/Application/Common/Abstractions/Errors/Sender.cs
@@ -0,0 +1,16 @@
+﻿using ErrorOr;
+
+namespace Application.Common.Abstractions.Errors
+{
+    public static partial class ApplicationErrors
+    {
+        public class Sender
+        {
+            public static Error HandlerNotFound() =>
+                Error.NotFound(
+                    code: "Sender.HandlerNotFound",
+                    description: "Handler not found for the given request."
+                );
+        }
+    }
+}
