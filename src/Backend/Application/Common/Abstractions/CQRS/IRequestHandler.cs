5878b2e7211346d0e78cf770ec1f689ef1de8fab
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2025-11-21 15:29:45 -0300

    Created handler for request
    unified commandHandler and queryHandler into a generic handler that accepts querys, commands with return type, and void commands
    
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

---
+++ src/Backend/Application/Common/Abstractions/CQRS/IRequestHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ErrorOr;
+
+namespace Application.Common.Abstractions.CQRS
+{
+    /// <summary>
+    /// Interfaz para el handler genérico de cada request.
+    /// </summary>
+    /// <typeparam name="TRequest">Comando o query</typeparam>
+    /// <typeparam name="TResponse">Tipo de respuesta</typeparam>
+    public interface IRequestHandler<in TRequest, TResponse>
+        where TRequest : IBaseRequest<TResponse>
+    {
+        Task<ErrorOr<TResponse>> HandleAsync(TRequest request,
+            CancellationToken cancellationToken = default);
+    }
+}
