0f59f5fb05e000a25a734e0963d4fa1d0f6b545c
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2025-11-21 18:31:01 -0300

    Created AnonymousUserCommand
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

---
+++ src/Backend/Application/Users/Commands/CreateAnonymousUser/CreateAnonymousUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common.Abstractions.CQRS;
+using Application.Common.DTOs;
+
+namespace Application.Users.Commands.CreateAnonymousUser
+{
+    /// <summary>
+    /// Comando para crear un usuario anónimo.
+    /// No tiene parámetros.
+    /// Devuelve un UserDto que representa al usuario anónimo creado.
+    /// </summary>
+    public sealed record CreateAnonymousUserCommand() : ICommand<UserDto>;
+    //record se utiliza para definir una clase que solo transporta datos
+    //sealed se utiliza para evitar que la clase sea heredada
+}
