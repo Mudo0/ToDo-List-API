b1e75b2721dce47a1110a7a122e7122bc724c6be
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2025-11-21 18:26:25 -0300

    Added a new error to the sender
    added a custom error for exceptions in the sender/mediator
    
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

--- src/Backend/Application/Common/Abstractions/Errors/Sender.cs
+++ src/Backend/Application/Common/Abstractions/Errors/Sender.cs
@@ -4,13 +4,33 @@ namespace Application.Common.Abstractions.Errors
 {
     public static partial class ApplicationErrors
     {
-        public class Sender
+        /// <summary>
+        /// Clase estática que define los errores del mediador / emisor.
+        /// </summary>
+        public static class Sender
         {
-            public static Error HandlerNotFound() =>
-                Error.NotFound(
+            /// <summary>
+            /// Error que indica que no se encontró un handler registrado para la request dada.
+            /// </summary>
+            /// <param name="requestName">Request del handler solicitado</param>
+            /// <returns>ErrOr type</returns>
+            public static Error HandlerNotFound(string requestName) =>
+                Error.Unexpected(
                     code: "Sender.HandlerNotFound",
-                    description: "Handler not found for the given request."
+                    description: $"No registered handler was found " +
+                                 $"for the given request: {requestName} "
                 );
+
+            /// <summary>
+            /// Error inesperado para las excepciones no controladas en el mediador.
+            /// </summary>
+            /// <param name="requestName">Request que se estaba procesando al momento de la excepción</param>
+            /// <returns>ErrOr type</returns>
+            public static Error UnhandledException(string requestName) =>
+                Error.Unexpected(
+                    code: "Sender.Exception",
+                    description: $"An unexpected error " +
+                                 $"occurred in the server while processing: {requestName}");
         }
     }
 }
